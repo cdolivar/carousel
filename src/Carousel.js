e84f96996bf335f244923708eb4d3d9527f9d872
Charles David Olivar <colivar@indeed.com>
2019-08-29 16:27:09 -0700

    to class component

--- src/Carousel.js
+++ src/Carousel.js
@@ -1,44 +1,64 @@
-import React from 'react';
+import React, { Component } from 'react';
 import './Carousel.css';
 import CarouselCard from './CarouselCard';
 
-function Carousel() {
-    return (
-        <div className="carousel-wrapper">
-            <div className='carousel-title'>
-                New jobs from your recent searches
+export default class Carousel extends Component {
+
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            activeIndex:0
+        };
+    }
+
+    render() {
+
+        const data = [
+            { what: "Nurse Practitioner", where: "San Francisco, CA", count: 345 },
+            { what: "Tattoo Artist", count: 345 },
+            { what: "Administrative Assistant", where: "San Francisco Bay Area", count: 45 }
+        ]
+
+        listItems = data.map(() => {
+            
+        });
+
+        return (
+            <div className="carousel-wrapper">
+                <div className='carousel-title'>
+                    New jobs from your recent searches
+                    </div>
+                <div className="carousel-scroll">
+                    <CarouselCard
+                        what="Firefighter"
+                        where="San Francisco, Ca"
+                        newCount={156}
+                    />
+                    <CarouselCard
+                        what="Firefighter"
+                        where="San Francisco, Ca"
+                        newCount={156}
+                    />
+                    <CarouselCard
+                        what="Firefighter"
+                        where="San Francisco, Ca"
+                        newCount={156}
+                    />
+                    <CarouselCard
+                        what="Firefighter"
+                        where="San Francisco, Ca"
+                        newCount={156}
+                    />
+                    <CarouselCard
+                        what="Firefighter"
+                        where="San Francisco, Ca"
+                        newCount={156}
+                    />
+                    <div className="right-margin"></div>
+                </div>
             </div>
-            <div className="carousel-scroll">
-                <CarouselCard
-                    what="Firefighter"
-                    where="San Francisco, Ca"
-                    newCount={156}
-                />
-                <CarouselCard
-                    what="Firefighter"
-                    where="San Francisco, Ca"
-                    newCount={156}
-                />
-                <CarouselCard
-                    what="Firefighter"
-                    where="San Francisco, Ca"
-                    newCount={156}
-                />
-                <CarouselCard
-                    what="Firefighter"
-                    where="San Francisco, Ca"
-                    newCount={156}
-                />
-                <CarouselCard
-                    what="Firefighter"
-                    where="San Francisco, Ca"
-                    newCount={156}
-                />
-                <div className="right-margin"></div>
-            </div>
-        </div>
-    );
+        );
+    }
+
 }
-
-
-export default Carousel;
